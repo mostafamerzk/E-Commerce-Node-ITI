c76405ca977fec6d3000441948e892a3a186559f
mostafamerzk <marzk756@gmail.com>
2026-02-20 21:28:33 +0200

    fix: entry point file

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ await connectionDB();
 await bootstrap(app, express);
 await connectionDB();
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(3000, () => {
-    console.log("server is running on port 3000");
-  });
-}
+// if (process.env.NODE_ENV !== "production") {
+//   app.listen(3000, () => {
+//     console.log("server is running on port 3000");
+//   });
+// }
 
 export default app;
