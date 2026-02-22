01374bc9fc21c8f33ff2b112bffdba723a935777
mostafamerzk <marzk756@gmail.com>
2026-02-22 18:14:01 +0200

    fix: fix port problem with railway

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const app = express();
 await bootstrap(app, express);
 await connectionDB();
 
-
 if (process.env.NODE_ENV !== "production") {
-  app.listen(3000, () => {
-    console.log("server is running on port 3000");
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
